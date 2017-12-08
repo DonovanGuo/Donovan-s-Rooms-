66f72ead87f8ed603b9d33433a7ad94209deeee4
BT_1N3_12 <BT_1N3_12@BT_1N3_12.bktech.local>
2017-12-08 09:26:12 -0500

    Heyo!

--- DonoVan's Rooms/src/Room.java
+++ DonoVan's Rooms/src/Room.java
@@ -10,12 +10,20 @@ public class Room {
 	}
 	public void enterRoom(Person x)
 	{
-		System.out.println("You enter a plain old room");
+		System.out.println("You enter the riddler's room, you must answer his questions to move!");
 		occupant = x;
 		x.setxLoc(this.xLoc);
 		x.setyLoc(this.yLoc);
 	}
-	
+	private String [] riddleeasy = {"What Has four legs initially, two afterwards, and three finally?", 
+									 "What Dissapears the moment it is said?",
+									 "What's brown and sticky?",
+									 "What can you never have for Breakfast"};
+	 
+	private String [] riddlemed = {"What taste better than it smells?",
+									"What animal asks but never answers",
+									"What loses its head in the morning but gets it back at night"};
+	private String [] riddlehard = {
 	public void leaveRoom(Person x)
 	{
 		occupant = null;
